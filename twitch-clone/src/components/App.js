ced96711dcf3dd5b768d647017471bd36c4d1732
Charlotte Weager <senwyn@googlemail.com>
2021-08-12 00:12:16 +0100

    Set up base header

--- twitch-clone/src/components/App.js
+++ twitch-clone/src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Header from './header';
 import StreamCreate from './streams/stream_create';
 import StreamDelete from './streams/stream_delete';
 import StreamEdit from './streams/stream_edit';
@@ -8,14 +9,15 @@ import StreamList from './streams/stream_list';
 
 const App = () => {
   return (
-    <div>
+    <div className="ui container">
       <BrowserRouter>
+        <Header />
         <div>
           <Route path="/" exact component={StreamList} />
+          <Route path="/streams/show" exact component={StreamShow} />
           <Route path="/streams/new" exact component={StreamCreate} />
           <Route path="/streams/edit" exact component={StreamEdit} />
           <Route path="/streams/delete" exact component={StreamDelete} />
-          <Route path="/streams/:id" exact component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
