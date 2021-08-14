8586f8051ca36cd68fef003b772211fe5fc5a8cb
Charlotte Weager <senwyn@googlemail.com>
2021-08-14 20:58:59 +0100

    implemented json server and post request

--- twitch-clone/src/actions/index.js
+++ twitch-clone/src/actions/index.js
@@ -1,3 +1,18 @@
+export const createStream = (formValues) => async (dispatch) => {
+  const response = await fetch(`http://localhost:3001/streams`,
+    {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      method: "POST",
+      body: JSON.stringify(formValues)
+    })
+  dispatch({ type: "CREATE_STREAM", payload: response.data })
+}
+
+
+
 export const signIn = (userId) => {
   return {
     type: 'SIGN_IN',
