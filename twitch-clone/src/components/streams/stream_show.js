7ce5fd33707727bc43ed424ec76472f807c09f71
Charlotte Weager <senwyn@googlemail.com>
2021-08-19 23:02:58 +0100

    Added video player to app

--- twitch-clone/src/components/streams/stream_show.js
+++ twitch-clone/src/components/streams/stream_show.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
+import flv from 'flv.js';
 import { connect } from 'react-redux';
 import { fetchStream } from '../../actions';
 
 class StreamShow extends Component {
-  componentDidMount() {
-    this.props.fetchStream(this.props.match.params.id);
+  constructor(props) {
+    super(props);
+    this.videoRef = React.createRef();
   }
+
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.fetchStream(id);
+    this.buildPlayer(id);
+  }
+
+  buildPlayer = (id) => {
+    if (this.player || !this.props.stream) {
+      return;
+    }
+    this.player = flv.createPlayer({
+      type: 'flv',
+      url: `http://localhost:8000/live/${id}.flv`
+    })
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
+  }s
 
   render() {
     if (!this.props.stream) {
@@ -15,6 +35,7 @@ class StreamShow extends Component {
     const { title, description } = this.props.stream
     return (
       <div>
+        <video ref={this.videoRef} style={{ width: '100%' }} controls />
         <h1>{title}</h1>
         <h5>{description}</h5>
       </div>
@@ -29,3 +50,6 @@ const mapStateToProps = (state, ownProps) => {
 }
 
 export default connect(mapStateToProps, { fetchStream })(StreamShow);
+
+
+// const NodeMediaServer = require('node-media-server');
